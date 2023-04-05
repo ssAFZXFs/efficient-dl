2349fe4d5a6d576db15bcaaf07e21b47e52b575b
Ziqiao <80455339+ssAFZXFs@users.noreply.github.com>
2023-04-05 11:34:48 +0200

    Add files via upload

---
+++ lab1-2.py
@@ -0,0 +1,175 @@
+from torchvision.datasets import CIFAR10
+import numpy as np 
+import torchvision.transforms as transforms
+import torch 
+from torch.utils.data.dataloader import DataLoader
+import models 
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim import Adam
+from torch.autograd import Variable
+import wandb
+
+## Normalization adapted for CIFAR10
+
+
+normalize_scratch = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+
+# Transforms is a list of transformations applied on the 'raw' dataset before the data is fed to the network. 
+# Here, Data augmentation (RandomCrop and Horizontal Flip) are applied to each batch, differently at each epoch, on the training set data only
+
+
+# Transforms train and test
+
+
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    normalize_scratch,
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    normalize_scratch,
+])
+
+### The data from CIFAR10 will be downloaded in the following folder
+rootdir = '/users/local/z20wang/datasets/'
+
+c10train = CIFAR10(rootdir,train=True,download=True,transform=transform_train)
+c10test = CIFAR10(rootdir,train=False,download=True,transform=transform_test)
+
+trainloader = DataLoader(c10train,batch_size=32,shuffle=True)
+testloader = DataLoader(c10test,batch_size=32) 
+
+
+# RandomSampler to simplify training
+
+
+## number of target samples for the final dataset
+num_train_examples = len(c10train)
+num_samples_subset = 15000
+
+## We set a seed manually so as to reproduce the results easily
+seed  = 2147483647
+
+## Generate a list of shuffled indices ; with the fixed seed, the permutation will always be the same, for reproducibility
+indices = list(range(num_train_examples))
+np.random.RandomState(seed=seed).shuffle(indices)## modifies the list in place
+
+## We define the Subset using the generated indices 
+c10train_subset = torch.utils.data.Subset(c10train,indices[:num_samples_subset])
+print(f"Initial CIFAR10 dataset has {len(c10train)} samples")
+print(f"Subset of CIFAR10 dataset has {len(c10train_subset)} samples")
+
+# Finally we can define anoter dataloader for the training data
+trainloader_subset = DataLoader(c10train_subset,batch_size=32,shuffle=True)
+### You can now use either trainloader (full CIFAR10) or trainloader_subset (subset of CIFAR10) to train your networks.
+
+
+# TASK 1. Train a model from scratch - DenseNet
+
+wandb.init(
+    # set the wandb project where this run will be logged
+    name="VGG11",
+    project="lab1",
+    entity='efficient-deeplearning',
+    # track hyperparameters and run metadata
+    config={
+    "learning_rate": 0.001,
+    "architecture": "VGG11",
+    "dataset": "CIFAR-100",
+    "epochs": 30,
+    }
+)
+
+model = models.VGG('VGG11')
+print('model loaded') 
+# Define the loss function with Classification Cross-Entropy loss and an optimizer with Adam optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
+
+# Function to save the model
+def saveModel():
+    path = "/homes/z20wang/Bureau/ML/efficient-deep-learning/outputs/models_trained/Model-VGG11-CIFAR10-1.pth"
+    torch.save(model.state_dict(), path)
+
+# Function to test the model with the test dataset and print the accuracy for the test images
+def testAccuracy():
+    
+    model.eval()
+    if torch.cuda.is_available():
+        model.cuda()
+    accuracy = 0.0
+    total = 0.0
+    
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
+            # run the model on the test set to predict labels
+            outputs = model(images)
+            # the label with the highest energy will be our prediction
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            accuracy += (predicted == labels).sum().item()
+    
+    # compute the accuracy over all test images
+    accuracy = (100 * accuracy / total)
+    return(accuracy)
+
+
+# Training function. We simply have to loop over our data iterator and feed the inputs to the network and optimize.
+def train(num_epochs):
+    
+    best_accuracy = 0.0
+    # Convert model parameters and buffers to CPU or Cuda
+    model.to(device)
+
+    for epoch in range(num_epochs):  # loop over the dataset multiple times
+        running_loss = 0.0
+        running_acc = 0.0
+
+        for i, (images, labels) in enumerate(trainloader_subset, 0):
+            
+            # get the inputs
+            images = Variable(images.to(device))
+            labels = Variable(labels.to(device))
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            # predict classes using images from the training set
+            outputs = model(images)
+            # compute the loss based on model output and real labels
+            loss = loss_fn(outputs, labels)
+            # backpropagate the loss
+            loss.backward()
+            # adjust parameters based on the calculated gradients
+            optimizer.step()
+
+            # Let's print statistics for every 1,000 images
+            running_loss += loss.item()     # extract the loss value
+            if i % 1000 == 999:    
+                # print every 1000 (twice per epoch) 
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 1000))
+                # zero the loss
+                running_loss = 0.0
+
+        # Compute and print the average accuracy fo this epoch when tested over all 1000 test images
+        accuracy = testAccuracy()
+        wandb.log({"acc": accuracy, "loss": running_loss / 1000})
+        print('For epoch', epoch+1,'the test accuracy over the whole test set is %d %%' % (accuracy))
+        
+        # we want to save the model if the accuracy is the best
+        if accuracy > best_accuracy:
+            saveModel()
+            best_accuracy = accuracy
+# Define your execution device
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print("The model will be running on", device, "device")
+train(30)
+wandb.finish()
+
+print('success!!!')
